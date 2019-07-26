67c5ecf279a263f755790821ff4c2bbb2dccd79e
Isra <garciamx@gmail.com>
2019-07-26 18:18:15 +0100

    stop if any of the arguments is missing

--- template.sh
+++ template.sh
@@ -16,6 +16,7 @@ template="$1"
 location="$2"
 
 test -z "$template" && help && exit 1
+test -z "$location" && help && exit 1
 test -a "$location" && error_nonempty_dir && exit 2
 
 TEMPLATES_LOC=$HOME/templates
